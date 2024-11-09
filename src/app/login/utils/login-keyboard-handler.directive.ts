4e62d44da9596e497f8e92bf15156954f92bf5be
andreyostroglyad <andrey@triber.app>
2024-11-09 16:41:33 +0200

    - login page fix for inputs

--- src/app/login/utils/login-keyboard-handler.directive.ts
+++ src/app/login/utils/login-keyboard-handler.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, OnInit, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Keyboard } from '@capacitor/keyboard';
 import { Capacitor } from '@capacitor/core';
 import { KeyboardResize } from '@capacitor/keyboard/dist/esm/definitions';
@@ -9,20 +9,21 @@ import { KeyboardResize } from '@capacitor/keyboard/dist/esm/definitions';
 })
 export class LoginKeyboardHandlerDirective implements OnInit, OnDestroy {
   private avatarElement: HTMLElement;
-  private loginButtonElement: HTMLElement;
+  private loginContainer: HTMLElement;
+  private loginForm: HTMLElement;
   private isMobilePlatform: boolean = Capacitor.getPlatform() !== 'web';
+  private isKeyboardVisible: boolean = false;
 
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   async ngOnInit(): Promise<void> {
-    await Keyboard.setResizeMode({ mode: KeyboardResize.None });
-
-    if (this.isMobilePlatform) {
+    setTimeout(() => {
       this.avatarElement = this.el.nativeElement.querySelector('.login100-form-avatar');
-      this.loginButtonElement = this.el.nativeElement.querySelector('.container-login100-form-btn');
+      this.loginContainer = this.el.nativeElement.querySelector('.container-login100');
+      this.loginForm = this.el.nativeElement.querySelector('.login100-form');
+    }, 0);
+    if (this.isMobilePlatform) {
+      await Keyboard.setResizeMode({ mode: KeyboardResize.None });
 
       Keyboard.addListener('keyboardDidShow', () => this.handleKeyboardShow());
       Keyboard.addListener('keyboardDidHide', () => this.handleKeyboardHide());
@@ -35,28 +36,53 @@ export class LoginKeyboardHandlerDirective implements OnInit, OnDestroy {
     }
   }
 
+  //needed only for testing on web (works on mac)
+  @HostListener('window:keydown.meta.k', ['$event'])
+  emulateKeyboardShow(event: KeyboardEvent): void {
+    event.preventDefault();
+    if (!this.isMobilePlatform && !this.isKeyboardVisible) {
+      this.handleKeyboardShow();
+      this.isKeyboardVisible = true;
+    }
+  }
+
+  //needed only for testing on web (works on mac)
+  @HostListener('window:keydown.meta.h', ['$event'])
+  emulateKeyboardHide(event: KeyboardEvent): void {
+    event.preventDefault();
+    if (!this.isMobilePlatform && this.isKeyboardVisible) {
+      this.handleKeyboardHide();
+      this.isKeyboardVisible = false;
+    }
+  }
+
   private handleKeyboardShow(): void {
+    if (this.loginForm) {
+      this.renderer.setStyle(this.loginForm, 'margin-top', '70%');
+    }
+
+    if (this.loginContainer) {
+      this.renderer.setStyle(this.loginContainer, 'align-items', 'unset');
+    }
+
     if (this.avatarElement) {
       this.renderer.setStyle(this.avatarElement, 'opacity', '0');
       this.renderer.setStyle(this.avatarElement, 'height', '0');
-      this.renderer.setStyle(this.avatarElement, 'display', 'none');
-    }
-
-    if (this.loginButtonElement) {
-      this.renderer.setStyle(this.loginButtonElement, 'position', 'absolute');
-      this.renderer.setStyle(this.loginButtonElement, 'bottom', '65px');
     }
   }
 
   private handleKeyboardHide(): void {
+    if (this.loginForm) {
+      this.renderer.setStyle(this.loginForm, 'margin-top', '0');
+    }
+
+    if (this.loginContainer) {
+      this.renderer.setStyle(this.loginContainer, 'align-items', 'center');
+    }
+
     if (this.avatarElement) {
       this.renderer.setStyle(this.avatarElement, 'opacity', '1');
       this.renderer.setStyle(this.avatarElement, 'height', '220px');
-    }
-
-    if (this.loginButtonElement) {
-      this.renderer.setStyle(this.loginButtonElement, 'position', 'relative');
-      this.renderer.setStyle(this.loginButtonElement, 'bottom', 'auto');
     }
   }
 }
